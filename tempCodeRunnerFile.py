0eb23231557e34d01066b5b99fc216ac88ead213
RavisriVK <cs19b081@smail.iitm.ac.in>
2022-12-09 21:40:26 +0530

    removing tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-   # writer.writerow(['images/chimpanzee.jpeg', 'What animal is this?'])
-    # writer.writerow(['images/donuts.png', 'What is under the donuts'])
-    # writer.writerow(['images/house-party.png', 'Are they drinking wine?'])
-    # writer.writerow(['images/panda-tree.png', 'Is the panda alone?'])
-    # writer.writerow(['images/pizza-g
